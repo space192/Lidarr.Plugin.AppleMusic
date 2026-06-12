55291ed00cbcf0e6f657f54b667a345e01c6bbba
space192 <arthur.fournier@mistral.ai>
2026-06-12 18:55:16 +0200

    fix: correct Owner and GithubUrl to space192

--- src/Lidarr.Plugin.AppleMusic/Plugin.cs
+++ src/Lidarr.Plugin.AppleMusic/Plugin.cs
@@ -3,7 +3,7 @@ namespace NzbDrone.Core.Plugins
     public class AppleMusicPlugin : Plugin
     {
         public override string Name => "AppleMusic";
-        public override string Owner => "arthMusic";
-        public override string GithubUrl => "https://github.com/arthMusic/Lidarr.Plugin.AppleMusic";
+        public override string Owner => "space192";
+        public override string GithubUrl => "https://github.com/space192/Lidarr.Plugin.AppleMusic";
     }
 }
